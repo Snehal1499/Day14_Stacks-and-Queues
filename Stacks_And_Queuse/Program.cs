1680f4cc5d3a94b0f3d2f84ed444780a22a9d63b
Snehal1499 <snehalpatil1718@gmail.com>
2022-03-24 22:44:41 +0530

    Added UC1 Stack And Queues Problem

---
+++ Stacks_And_Queuse/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Stacks_And_Queuse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pushing values to Stack");
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.push(70);
+            linkedListStack.push(30);
+            linkedListStack.push(56);
+            linkedListStack.Display();
+
+        }
+    }
+}
