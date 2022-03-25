ea3f6fdc2114a155ebadc29a0db896a5d427d81d
Snehal1499 <snehalpatil1718@gmail.com>
2022-03-25 18:01:33 +0530

    Added UC2 Peak And Pop Method

---
+++ Stacks_And_Queues/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Stacks_And_Queues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pushing values to Stack");
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.push(70);
+            linkedListStack.push(30);
+            linkedListStack.push(56);
+            linkedListStack.Display();
+            Console.WriteLine("Using Peek and Pop Till Stack is Empty");
+            linkedListStack.Peek();
+            linkedListStack.Pop();
+            linkedListStack.IsEmpty();
+            linkedListStack.Display();
+
+        }
+    }
+}
